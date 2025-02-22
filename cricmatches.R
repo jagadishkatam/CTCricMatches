6e48d167c2e9b645006fd541daec0f6ea251b231
jagadishkatam <jagadish.katam@gmail.com>
2025-02-22 14:15:02 -0500

    initial commit

---
+++ cricmatches.R
@@ -0,0 +1,22 @@
+
+library(googlesheets4)
+library(timevis)
+library(htmlwidgets)
+
+# Replace with your Google Sheet URL or Sheet ID
+sheet_url <- "https://docs.google.com/spreadsheets/d/19SGRA-yeXmTxM-G24HiyQBAoXyvWJtzQyqeaI-i1C3M/edit?gid=0#gid=0"
+
+# Read the entire sheet
+df <- read_sheet(sheet_url)
+
+# View data
+head(df)
+
+tv <- timevis(df,
+              options = list(
+                format = htmlwidgets::JS("{ minorLabels: { minute: 'h:mma', hour: 'ha' }}")
+              ))
+
+
+# Save the output as an HTML file
+saveWidget(tv, "timeline.html", selfcontained = TRUE)
