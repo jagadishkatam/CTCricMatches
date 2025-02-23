b2e5cb1dd9d076abe9449061296393523c6fdd58
Jagadish K <83913148+jagadishkatam@users.noreply.github.com>
2025-02-23 15:16:11 -0500

    Update cricmatches.R

--- cricmatches.R
+++ cricmatches.R
@@ -37,5 +37,5 @@ tv <- timevis(df,width = "100%",
 saveWidget(tv, "timeline.html", selfcontained = TRUE)
 
 # Take a screenshot and save as an image
-webshot("timeline.html", file = "timeline.png", cliprect = "viewport",vwidth = 2000, vheight = 2200)
+webshot("timeline.html", file = "timeline.png", cliprect = "viewport",vwidth = 1700, vheight = 1800)
 
