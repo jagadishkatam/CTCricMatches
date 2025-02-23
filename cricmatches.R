2245f02a91f8ed192bdf5f61b2c069edaeae86a3
Jagadish K <83913148+jagadishkatam@users.noreply.github.com>
2025-02-23 14:17:21 -0500

    Update cricmatches.R

--- cricmatches.R
+++ cricmatches.R
@@ -11,8 +11,11 @@ gs4_auth(path = Sys.getenv("GOOGLE_SHEET_CREDENTIALS"))
 # Replace with your Google Sheet URL or Sheet ID
 sheet_url <- "https://docs.google.com/spreadsheets/d/19SGRA-yeXmTxM-G24HiyQBAoXyvWJtzQyqeaI-i1C3M/edit?gid=0#gid=0"
 
-# Read the entire sheet
-df <- read_sheet(sheet_url)
+
+groups <- df |> select(group,groups) |>  arrange(group) |> group_by(group) |> slice_head(n=1) |> ungroup() |> 
+  mutate(content =if_else(!groups %in% c('Semi','final'), paste('Group', if_else(group==1,'A','B')), groups),
+         id=row_number()
+         ) |> select(-groups) 
 
 # View data
 # head(df)
@@ -20,7 +23,9 @@ df <- read_sheet(sheet_url)
 tv <- timevis(df,width = "100%",
               options = list(
                 format = htmlwidgets::JS("{ minorLabels: { minute: 'h:mma', hour: 'ha' }}")
-              ))
+              ),
+              groups = groups
+              )
 
 
 # Save the output as an HTML file
